aaafd2f3cecbb82c6ce87d114a33eb2a175e13e3
Josna Johnson <131433610+JosnaJohnson29@users.noreply.github.com>
2026-08-07 15:31:54 +0530

    Fix typo in temperature label variable name

--- venv/demo_temp.py
+++ venv/demo_temp.py
@@ -40,8 +40,8 @@ wb_Label.place(x=25, y=330, height=50, width=210)
 wb_Label1 = Label(win,text="",font=("Time New Romen",17))
 wb_Label1.place(x=250, y=330, height=50, width=210)
 
-tem_Label = Label(win,text="Temperature",font=("Time New Romen",20,))
-tem_Label.place(x=25, y=400, height=50, width=210)
+temp_Label = Label(win,text="Temperature",font=("Time New Romen",20,))
+temp_Label.place(x=25, y=400, height=50, width=210)
 
 tem_Label1 = Label(win,text="",font=("Time New Romen",20,))
 tem_Label1.place(x=250, y=400, height=50, width=210)
